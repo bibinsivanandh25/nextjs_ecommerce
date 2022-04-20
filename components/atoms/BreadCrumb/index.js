f3805006c3a06b4acbe05775d334d2c69d4808b5
TL_Shambhavi_V <shambhavi.v@testyantra.in>
2022-04-20 15:13:20 +0530

    Update breadcurmbs

--- components/atoms/BreadCrumb/index.js
+++ components/atoms/BreadCrumb/index.js
@@ -6,6 +6,140 @@ import { useRouter } from "next/router";
 import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 
 const BreadCrumb = () => {
+  const paths = [
+    {
+      title: "Accept & Confirm Address (00)",
+      id: "acceptandconfirmaddress",
+    },
+    {
+      title: "Generate Invoice & Manifest (00)",
+      id: "ordersdata",
+    },
+    {
+      title: "Upload Manifest (00)",
+      id: "manifestupload",
+    },
+    {
+      title: "Manifested Orders (00)",
+      id: "manifestedorders",
+    },
+    {
+      title: "Shipped Orders (00)",
+      id: "shippedorders",
+    },
+    {
+      title: "Delivered Orders (00)",
+      id: "deliveredorders",
+    },
+    {
+      title: "Cancelled Orders (00)",
+      id: "cancelledorders",
+    },
+    {
+      title: "Returned Orders (00)",
+      id: "returnedorders",
+    },
+    {
+      title: "My Products (Update Inventory)",
+      id: "myproduct",
+    },
+    {
+      title: "Add New Products",
+      id: "newproducts",
+    },
+    {
+      title: "Excel Upload (New Products Upload)",
+      id: "excel_upload",
+    },
+    {
+      title: "Add Inventory (Excel)",
+      id: "add_Inventory",
+    },
+    {
+      title: "MrMrsCart Product",
+      id: "MrMrsCartProduct",
+    },
+    {
+      title: "Payment Reports",
+      id: "payment-report",
+    },
+    {
+      title: "Order Report",
+      id: "order-report",
+    },
+    {
+      title: "Order Tax Invoice Report",
+      id: "order-tex-invoice-report",
+    },
+    {
+      title: "Credit Notes",
+      id: "credit-notes",
+    },
+    {
+      title: "Commission Invoices",
+      id: "commission-invoices",
+    },
+    {
+      title: "TCS/Sales Report",
+      id: "sales-report",
+    },
+    {
+      id: "dashboard",
+      title: "Dashboard",
+    },
+    {
+      id: "myorders",
+      title: "My Orders",
+    },
+    {
+      id: "collections",
+      title: "My Collections",
+    },
+    {
+      id: "products",
+      title: "Products & Inventory",
+    },
+    {
+      id: "earnings",
+      title: "My Earnings",
+    },
+    {
+      id: "adminproducts",
+      title: "Admin Products",
+    },
+    {
+      id: "coupns",
+      title: "Coupons",
+    },
+    {
+      id: "staff",
+      title: "Staff",
+    },
+    {
+      id: "delivery",
+      title: "Delivery Management",
+    },
+    {
+      id: "review",
+      title: "Customer Review",
+    },
+    {
+      id: "reports",
+      title: "Reports",
+    },
+    {
+      id: "help",
+      title: "Help & Support",
+    },
+    {
+      title: "New Orders To Process (0)",
+      id: "neworders",
+    },
+    {
+      title: "Order History (0)",
+      id: "orderhistory",
+    },
+  ];
   const route = useRouter();
   let completePath = "/";
   const path =
@@ -34,7 +168,7 @@ const BreadCrumb = () => {
                     path.length === index + 1 ? "color-orange" : ""
                   } fs-14 mx-2`}
                 >
-                  {item}
+                  {paths.find((i) => i.id === item)?.title}
                 </span>
               </Link>
             </React.Fragment>
