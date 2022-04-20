b873f6a192690bfc86016a2f867fe3af45b6b50e
TY_RakeshKumar <rakesh.k@testyantra.in>
2022-04-20 15:27:48 +0530

    created my order components

--- components/atoms/InputBoxComponent/index.js
+++ components/atoms/InputBoxComponent/index.js
@@ -1,55 +1,54 @@
 import { TextField } from "@mui/material";
-import InputAdornment from '@mui/material/InputAdornment';
-import IconButton from '@mui/material/IconButton';
-import VisibilityIcon from '@mui/icons-material/Visibility';
+import InputAdornment from "@mui/material/InputAdornment";
+import IconButton from "@mui/material/IconButton";
+import VisibilityIcon from "@mui/icons-material/Visibility";
 import { Visibility, VisibilityOff } from "@mui/icons-material";
 
-const InputBox= ({
-    placeholder='placeholder',
-    value='default value',
-    label='label',
-    className='w-25',
-    size='small',
-    onInputChange =()=>{},
-    iconName = '',
-    onIconClick = ()=>{}
-    })=>{
-
-        const getIcons =()=>{
-            if(iconName==='visible'){
-                return <Visibility  />
-            }
-           else if(iconName === 'visibleOff'){
-                return <VisibilityOff/>
-            }
-            else {
-                return null;
-            }
-
-        }
-    return (
-        <TextField
-        onChange={
-        onInputChange
-        }
-        label={label}
-        placeholder={placeholder}
-        defaultValue={value}
-        size={size}
-        className={className}
-        InputProps={{
-            endAdornment: <InputAdornment position="end">
-                <IconButton
-                onClick={()=>{
-                    onIconClick()
-                }}
-                >
-                {getIcons()}
-                </IconButton>
-                </InputAdornment>,
-          }}
-        />
-    )
-
-}
+const InputBox = ({
+  placeholder = "",
+  inputlabelshrink = false,
+  value = "",
+  label = "",
+  className = "",
+  size = "small",
+  onInputChange = () => {},
+  iconName = "",
+  onIconClick = () => {},
+}) => {
+  const getIcons = () => {
+    if (iconName === "visible") {
+      return <Visibility />;
+    } else if (iconName === "visibleOff") {
+      return <VisibilityOff />;
+    } else {
+      return null;
+    }
+  };
+  return (
+    <TextField
+      onChange={onInputChange}
+      label={label}
+      placeholder={placeholder}
+      defaultValue={value}
+      size={size}
+      className={className}
+      InputLabelProps={{
+        shrink: { inputlabelshrink },
+      }}
+      InputProps={{
+        endAdornment: (
+          <InputAdornment position="end">
+            <IconButton
+              onClick={() => {
+                onIconClick();
+              }}
+            >
+              {getIcons()}
+            </IconButton>
+          </InputAdornment>
+        ),
+      }}
+    />
+  );
+};
 export default InputBox;
