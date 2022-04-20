d71b9112af25504b7ef1ea923c45fef9647e393c
TE_Suhil <suhil.km@testyantra.in>
2022-04-20 15:39:31 +0530

    Converted checkbox to controlled component

--- components/atoms/CheckboxComponent/index.js
+++ components/atoms/CheckboxComponent/index.js
@@ -1,10 +1,11 @@
 import CheckBoxIcon from "@mui/icons-material/CheckBox";
 import Checkbox from "@mui/material/Checkbox";
 import CheckBoxOutlineBlankIcon from "@mui/icons-material/CheckBoxOutlineBlank";
+import { FormControlLabel } from "@mui/material";
 
 const CheckBoxComponent = ({
   label = "",
-  isDefaultChecked = false,
+  isChecked = false,
   isDisabled = false,
   indeterminate = false,
   checkBoxClick = () => {},
@@ -13,24 +14,33 @@ const CheckBoxComponent = ({
 }) => {
   return (
     <div>
-      <Checkbox
-        label={label}
-        defaultChecked={isDefaultChecked}
-        icon={
-          varient === "filled" ? <CheckBoxIcon /> : <CheckBoxOutlineBlankIcon />
+      <FormControlLabel
+        label=""
+        control={
+          <Checkbox
+            label={label}
+            checked={isChecked}
+            icon={
+              varient === "filled" ? (
+                <CheckBoxIcon />
+              ) : (
+                <CheckBoxOutlineBlankIcon />
+              )
+            }
+            checkedIcon={<CheckBoxIcon />}
+            sx={{
+              "&.Mui-checked": {
+                color: "#e56700",
+              },
+            }}
+            disabled={isDisabled}
+            indeterminate={indeterminate}
+            onChange={(e) => {
+              checkBoxClick(e.target.checked);
+            }}
+            size={size}
+          />
         }
-        checkedIcon={<CheckBoxIcon />}
-        sx={{
-          "&.Mui-checked": {
-            color: "#e56700",
-          },
-        }}
-        disabled={isDisabled}
-        indeterminate={indeterminate}
-        onChange={(e) => {
-          checkBoxClick(e.target.checked);
-        }}
-        size={size}
       />
     </div>
   );
