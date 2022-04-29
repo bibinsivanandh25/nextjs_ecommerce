dd467d0f053dd3396de707c7d8237c593ef23c04
TL_Shambhavi_V <shambhavi.v@testyantra.in>
2022-04-29 17:56:25 +0530

    Add multiline props

--- components/atoms/InputBoxComponent/index.js
+++ components/atoms/InputBoxComponent/index.js
@@ -4,6 +4,8 @@ import IconButton from "@mui/material/IconButton";
 import { Visibility, VisibilityOff } from "@mui/icons-material";
 
 const InputBox = ({
+  id = "textbox",
+  name = "textbox",
   placeholder = "",
   inputlabelshrink = false,
   value = "",
@@ -18,6 +20,8 @@ const InputBox = ({
   onKeyDown = () => {},
   inputRef = null,
   type = "text",
+  isMultiline = false,
+  rows = 3,
 }) => {
   const getIcons = () => {
     if (iconName === "visible") {
@@ -37,12 +41,16 @@ const InputBox = ({
       size={size}
       className={className}
       InputLabelProps={{
-        shrink: { inputlabelshrink },
+        shrink: inputlabelshrink || value,
       }}
       inputRef={inputRef}
       onKeyDown={onKeyDown}
       variant={variant}
       type={type}
+      id={id}
+      name={name}
+      multiline={isMultiline}
+      rows={rows}
       InputProps={{
         endAdornment: iconName !== "" && (
           <InputAdornment position="end">
