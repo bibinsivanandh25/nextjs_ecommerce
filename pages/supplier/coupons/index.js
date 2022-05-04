f3a9d5b3d1e388053f29c6effc96af0900ffffbc
TL_Shambhavi_V <shambhavi.v@testyantra.in>
2022-05-04 17:20:45 +0530

    Update table filter in coupons

--- pages/supplier/coupons/index.js
+++ pages/supplier/coupons/index.js
@@ -10,15 +10,25 @@ import CouponLogo from "public/assets/images/Coupon.png";
 import Image from "next/image";
 
 const Coupons = () => {
+  const selectTypeList = [
+    {
+      id: "All",
+      label: "All",
+    },
+    {
+      id: "Scratch Card",
+      label: "Scratch Card",
+    },
+    {
+      id: "Fixed Product Discount",
+      label: "Fixed Product Discount",
+    },
+  ];
   const router = useRouter();
   const [tableRows, setTableRows] = useState([]);
-  const [showCreateComponent, setShowCreateComponent] = useState(false);
   const [tableData, setTableData] = useState([]);
   const [showPublishModal, setShowPublishModal] = useState(false);
-  const [showModal, setShowModal] = useState({
-    show: false,
-    id: null,
-  });
+  const [dropdownFilter, setDropdownFilter] = useState({});
   const columns = [
     {
       label: "Coupon Code",
@@ -56,6 +66,31 @@ const Coupons = () => {
     }
     return "";
   };
+
+  const filterByType = React.useCallback(() => {
+    if (dropdownFilter && dropdownFilter.id) {
+      switch (dropdownFilter?.id) {
+        case "Fixed Product Discount":
+          setTableRows(
+            tableRows?.filter((row) => row["col2"] === "Fixed Product Discount")
+          );
+          break;
+        case "Scratch Card":
+          setTableRows(
+            tableRows?.filter((row) => row["col2"] === "Scratch Card")
+          );
+          break;
+        default:
+          setTableRows(mapRowsToTable(tableData));
+      }
+    } else {
+      setTableRows(mapRowsToTable(tableData));
+    }
+  }, [dropdownFilter]);
+
+  useEffect(() => {
+    filterByType();
+  }, [dropdownFilter]);
 
   const mapRowsToTable = (data) => {
     const result = [];
@@ -105,7 +140,7 @@ const Coupons = () => {
       },
       {
         couponcode: "#123459",
-        discounttype: "Scratch Card",
+        discounttype: "Fixed Product Discount",
         expiredate: "12-01-2023, 08:43 PM",
         amount: "1",
         status: "Not Published",
@@ -120,73 +155,64 @@ const Coupons = () => {
   }, [tableData]);
 
   return (
-    <>
-      {showCreateComponent ? (
-        <HelpandsupportCreate />
-      ) : showModal.show && showModal.type === "view" ? (
-        <HelpandsupportView />
-      ) : (
-        <Paper>
-          <Grid container>
-            <Grid
-              container
-              item
-              xs={12}
-              justifyContent="space-between"
-              className="border-bottom"
-            >
-              <Grid item sx={{ p: 2 }} className="fs-16 fw-bold px-3">
-                Coupon Listing
-              </Grid>
-              <Grid item sx={{ p: 2 }}>
-                <Button
-                  variant="contained"
-                  className="bg-orange"
-                  size="small"
-                  onClick={() => router.push("/supplier/coupons/addnewcoupons")}
-                >
-                  Add New Coupon
-                </Button>
-              </Grid>
-            </Grid>
-            <Grid item xs={12} sx={{ my: 5, px: 2 }}>
-              <Paper>
-                <TableComponent
-                  table_heading=""
-                  columns={columns}
-                  tableRows={tableRows}
-                  showCheckbox={false}
-                  //   showSearchFilter={false}
-                />
-              </Paper>
-            </Grid>
-            {showModal.show && showModal.type === "notification" && (
-              <HelpAndSupportNotification
-                show={showModal.show}
-                setShowModal={setShowModal}
-              />
-            )}
-            {showPublishModal && (
-              <ModalComponent
-                showClearBtn={false}
-                open={showPublishModal}
-                ModalTitle=""
-                saveBtnText="Publish Coupon"
-                ModalWidth={600}
-                headerClassName="border-0"
-                onCloseIconClick={() => setShowPublishModal(false)}
-                onSaveBtnClick={() => setShowPublishModal(false)}
-                footerClassName="m-2 align-center"
-              >
-                <div className="d-flex flex-column justify-content-center align-items-center my-2">
-                  <Image src={CouponLogo} height={200} width={300} />
-                </div>
-              </ModalComponent>
-            )}
+    <Paper>
+      <Grid container>
+        <Grid
+          container
+          item
+          xs={12}
+          justifyContent="space-between"
+          className="border-bottom"
+        >
+          <Grid item sx={{ p: 2 }} className="fs-16 fw-bold px-3">
+            Coupon Listing
           </Grid>
-        </Paper>
-      )}
-    </>
+          <Grid item sx={{ p: 2 }}>
+            <Button
+              variant="contained"
+              className="bg-orange"
+              size="small"
+              onClick={() => router.push("/supplier/coupons/addnewcoupons")}
+            >
+              Add New Coupon
+            </Button>
+          </Grid>
+        </Grid>
+        <Grid item xs={12} sx={{ my: 5, px: 2 }}>
+          <Paper>
+            <TableComponent
+              table_heading=""
+              columns={columns}
+              tableRows={tableRows}
+              showCheckbox={false}
+              showSearchFilter={false}
+              showCustomDropdown
+              onCustomDropdownChange={(val) => setDropdownFilter(val)}
+              customDropdownValue={dropdownFilter}
+              customDropdownLabel="Select Type"
+              customDropdownList={selectTypeList}
+            />
+          </Paper>
+        </Grid>
+        {showPublishModal && (
+          <ModalComponent
+            showClearBtn={false}
+            open={showPublishModal}
+            ModalTitle=""
+            saveBtnText="Publish Coupon"
+            ModalWidth={600}
+            headerClassName="border-0"
+            onCloseIconClick={() => setShowPublishModal(false)}
+            onSaveBtnClick={() => setShowPublishModal(false)}
+            footerClassName="m-2 align-center"
+          >
+            <div className="d-flex flex-column justify-content-center align-items-center my-2">
+              <Image src={CouponLogo} height={200} width={300} />
+            </div>
+          </ModalComponent>
+        )}
+      </Grid>
+    </Paper>
   );
 };
 
