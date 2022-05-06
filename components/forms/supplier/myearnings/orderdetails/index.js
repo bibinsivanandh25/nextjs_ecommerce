c5529db14c8c26b457ebdac181afbadcf69f781c
TY_RakeshKumar <rakesh.k@testyantra.in>
2022-05-06 19:09:35 +0530

    updated order details Modal

--- components/forms/supplier/myearnings/orderdetails/index.js
+++ components/forms/supplier/myearnings/orderdetails/index.js
@@ -1,4 +1,4 @@
-import { Grid } from "@mui/material";
+import { Card, Grid } from "@mui/material";
 import ModalComponent from "components/atoms/ModalComponent";
 import Image from "next/image";
 import image from "public/assets/images/coupon.png";
@@ -75,34 +75,40 @@ const OrderDetails = () => {
       ModalTitle="Order Details (03 May 2021 to 02 Jun 2021)"
       minHeightClassName="mxh-600"
     >
-      <Grid container>
+      <Grid container spacing={2} className="mt-2   ">
         {orderdetails.map((ele, ind) => {
           return (
             <Grid
               item
               md={4}
               container
-              className={`border`}
+              //   className={`border`}
               key={ind}
-              //   spacing={1}
+              spacing={1}
             >
-              <Grid item md={3} className="mx-2">
-                <Image src={image} height={100} width={100} />
-              </Grid>
-              <Grid item md={8}>
-                <Grid className="color-orange fs-10">
-                  OrderId:{ele.OrderId}
-                </Grid>
-                <Grid className="fs-11 fw-bold ">{ele.Name}</Grid>
-                <Grid className="fs-11  ">Price:{ele.price}</Grid>
-                <Grid className="fs-11 ">Quantity:{ele.quantity}</Grid>
-                <Grid className="d-flex justify-content-between align-items-center mb-2">
-                  <Grid className="fs-11 text-success">Order Delivered</Grid>
-                  <Grid className="bg-orange fs-11 p-2 text-white">
-                    You Earned Rs.500
+              <Card variant="outlined">
+                <Grid container>
+                  <Grid item md={3} className="mx-2">
+                    <Image src={image} />
+                  </Grid>
+                  <Grid item md={8}>
+                    <Grid className="color-orange fs-10">
+                      OrderId:{ele.OrderId}
+                    </Grid>
+                    <Grid className="fs-11 fw-bold ">{ele.Name}</Grid>
+                    <Grid className="fs-11  ">Price:{ele.price}</Grid>
+                    <Grid className="fs-11 ">Quantity:{ele.quantity}</Grid>
+                    <Grid className="d-flex justify-content-between align-items-center mb-2 ">
+                      <Grid className="fs-11 text-success">
+                        Order Delivered
+                      </Grid>
+                      <Grid className="bg-orange fs-11 p-2 text-white">
+                        You Earned Rs.500
+                      </Grid>
+                    </Grid>
                   </Grid>
                 </Grid>
-              </Grid>
+              </Card>
             </Grid>
           );
         })}
