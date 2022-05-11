d3e723b77cdb74e4a5afb4fc840b8f07f5bb7611
TL_Shambhavi_V <shambhavi.v@testyantra.in>
2022-05-11 12:16:23 +0530

    Add Myproducts component

---
+++ components/molecule/BadgeComponent/index.js
@@ -0,0 +1,27 @@
+import * as React from "react";
+import { red } from "@mui/material/colors";
+import Badge from "@mui/material/Badge";
+import { styled } from "@mui/material/styles";
+
+const StyledBadge = styled(Badge)(({ theme }) => ({
+  "& .MuiBadge-badge": {
+    alignItems: "center",
+    border: `2px solid ${theme.palette.background.paper}`,
+    padding: "0 4px",
+  },
+}));
+
+export default function BadgeComponent({ count, color }) {
+  return (
+    <StyledBadge
+      badgeContent={count}
+      sx={{
+        "& .MuiBadge-badge": {
+          color: "white",
+          backgroundColor: color,
+        },
+        ml: 1.5,
+      }}
+    />
+  );
+}
