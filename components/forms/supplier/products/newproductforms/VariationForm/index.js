442b852b929b3d71eba6d1f26a6da26b1d9a9980
TL_Shambhavi_V <shambhavi.v@testyantra.in>
2022-05-13 15:18:39 +0530

    Update variation form data

--- components/forms/supplier/products/newproductforms/VariationForm/index.js
+++ components/forms/supplier/products/newproductforms/VariationForm/index.js
@@ -1,8 +1,23 @@
-import { Box, Grid, Paper } from "@mui/material";
-import { forwardRef, useImperativeHandle, useState } from "react";
+/* eslint-disable react-hooks/exhaustive-deps */
+import { Grid, Typography } from "@mui/material";
+import { forwardRef, useEffect, useImperativeHandle, useState } from "react";
+import SimpleDropdownComponent from "components/atoms/SimpleDropdownComponent";
+import DatePickerComponent from "components/atoms/DatePickerComponent";
+import InputBox from "components/atoms/InputBoxComponent";
 
-const VariationForm = forwardRef(({}, ref) => {
-  const [variationFormData, setVariationFormData] = useState({});
+const VariationForm = forwardRef(({ formData = {} }, ref) => {
+  const [variationFormData, setVariationFormData] = useState({
+    expiryDate: null,
+    others: "",
+    color: null,
+    fabric: null,
+    type: null,
+    availabeSizes: null,
+    style: null,
+    designType: null,
+    styleCode: null,
+    countryOfOrigin: null,
+  });
   useImperativeHandle(ref, () => {
     return {
       handleSendFormData: () => {
@@ -10,9 +25,197 @@ const VariationForm = forwardRef(({}, ref) => {
       },
     };
   });
+
+  useEffect(() => {
+    setVariationFormData({ ...formData.inventory });
+  }, [formData]);
+
+  useEffect(() => {
+    const dropdownCopy = [...dropdowns];
+    Object.entries(variationFormData).forEach(([key, value]) => {
+      const existingVal = dropdowns.find((i) => i.id === key);
+      const index = dropdowns.findIndex((i) => i.id === key);
+      dropdownCopy[index] = {
+        ...existingVal,
+        value,
+      };
+    });
+    setDropdowns(dropdownCopy);
+  }, [variationFormData]);
+
+  const [dropdowns, setDropdowns] = useState([
+    {
+      label: "Select Color",
+      type: "dropdown",
+      id: "color",
+      options: [
+        {
+          id: "blue",
+          label: "Blue",
+        },
+        { id: "black", label: "Black" },
+      ],
+      value: null,
+    },
+    {
+      label: "Select Fabric*",
+      type: "dropdown",
+      id: "fabric",
+      options: [
+        {
+          id: "cotton",
+          label: "Cotton",
+        },
+        { id: "silk", label: "Silk" },
+      ],
+      value: null,
+    },
+    {
+      label: "Select Type",
+      type: "dropdown",
+      id: "type",
+      options: [
+        {
+          id: "shirt",
+          label: "Shirt",
+        },
+        { id: "pant", label: "Pant" },
+      ],
+      value: null,
+    },
+    {
+      label: "Select Available Sizes*",
+      type: "dropdown",
+      id: "availabeSizes",
+      options: [
+        {
+          id: "m",
+          label: "Medium",
+        },
+        { id: "l", label: "Large" },
+      ],
+      value: null,
+    },
+    {
+      label: "Select Style",
+      type: "dropdown",
+      id: "style",
+      options: [
+        {
+          id: "spotted",
+          label: "Spotted",
+        },
+        { id: "striped", label: "Striped" },
+      ],
+      value: null,
+    },
+    {
+      label: "Select Design Type",
+      type: "dropdown",
+      id: "designType",
+      options: [
+        {
+          id: "blue",
+          label: "Blue",
+        },
+        { id: "black", label: "Black" },
+      ],
+      value: null,
+    },
+    {
+      label: "Style Code(optional)",
+      type: "dropdown",
+      id: "styleCode",
+      options: [
+        {
+          id: "blue",
+          label: "Blue",
+        },
+        { id: "black", label: "Black" },
+      ],
+      value: null,
+    },
+    {
+      label: "Expiry Date",
+      type: "date",
+      id: "expiryDate",
+      value: null,
+    },
+    {
+      label: "Country of Origin",
+      type: "dropdown",
+      id: "countryOfOrigin",
+      options: [
+        {
+          id: "india",
+          label: "India",
+        },
+        { id: "japan", label: "Japan" },
+      ],
+      value: null,
+    },
+    {
+      label: "Others",
+      type: "textarea",
+      id: "others",
+      value: null,
+    },
+  ]);
+
+  const handleInputChange = (val, ele) => {
+    const getData = () => {
+      if (ele.type === "dropdown") {
+        return val ? val.id : null;
+      } else {
+        return val;
+      }
+    };
+    setVariationFormData((prev) => {
+      return {
+        ...prev,
+        [ele.id]: getData(),
+      };
+    });
+  };
+
   return (
-    <Grid container className="">
-      VariationForm
+    <Grid container spacing={2} className="">
+      {dropdowns.map((ele) => {
+        return (
+          <Grid item md={12} container key={ele.id}>
+            <Grid item md={3}>
+              <Typography fontSize={14}>{ele.label}</Typography>
+            </Grid>
+            <Grid item lg={9} md={12} xs={12}>
+              {ele.type === "dropdown" && (
+                <SimpleDropdownComponent
+                  id={ele.id}
+                  size="small"
+                  list={ele.options}
+                  value={ele.value}
+                  onDropdownSelect={(val) => handleInputChange(val, ele)}
+                />
+              )}
+              {ele.type === "textarea" && (
+                <InputBox
+                  id={ele.id}
+                  value={ele.value}
+                  isMultiline
+                  onInputChange={(e) => handleInputChange(e.target.value, ele)}
+                />
+              )}
+              {ele.type === "date" && (
+                <DatePickerComponent
+                  id={ele.id}
+                  value={ele.value}
+                  size="small"
+                  onDateChange={(val) => handleInputChange(val, ele)}
+                />
+              )}
+            </Grid>
+          </Grid>
+        );
+      })}
     </Grid>
   );
 });
