16e5b3d32ca155c3dbce722083f2fb625b97ac9c
TY_RakeshKumar <rakesh.k@testyantra.in>
2022-05-19 15:43:54 +0530

    added validation to linked form and help and support page

--- components/atoms/TextEditor/index.js
+++ components/atoms/TextEditor/index.js
@@ -7,41 +7,26 @@ const SunEditor = dynamic(() => import("suneditor-react"), {
   ssr: false,
 });
 
-export default function TextEditor({ EditorHeight = "100px" }) {
-  const [content, setContent] = useState("");
-
-  // useEffect(() => {
-  //   // if (typeof window !== undefined) {
-  //   console.log(document.getElementsByClassName("sun-editor"));
-  //   let element = document.getElementsByClassName("sun-editor");
-  //   element.classList.add(styles.toolBarClassName);
-  //   // }
-  // }, []);
-  console.log(content, " <img>asds</img>", "content");
-  // useEffect(() => {
-  //   let element = document.getElementsByClassName("se-btn-tray");
-  //   element.length > 0 ? element.classList.remove("se-btn-tray") : null;
-  // }, []);
+export default function TextEditor({
+  EditorHeight = "100px",
+  getContent = () => {},
+  content = "",
+}) {
   return (
     <div className="w-95p">
-      {/* <Image src={logo} alt="" width="100px" height="40px" /> */}
-
       <SunEditor
         className="bg-primary"
         height={EditorHeight}
         placeholder="Add a Reply..."
         name="content"
         defaultValue={content}
-        onChange={(text) => setContent(text)}
+        onChange={(text) => {
+          getContent(text);
+        }}
         setOptions={{
           showPathLabel: false,
           resizingBar: false,
-          // paragraphStyles: [
-          //   {
-          //     name: "Box",
-          //     class: styles.toolBarClassName,
-          //   },
-          // ],
+
           colorList: [
             // ["#ff0000", "#ff5e00", "#ffe400", "#abf200"],
             // ["#00d8ff", "#0055ff", "#6600ff", "#ff00dd"],
