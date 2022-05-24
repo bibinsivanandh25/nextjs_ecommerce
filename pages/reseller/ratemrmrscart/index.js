6d72a5a9c0e722e2f7c66caa8f4f7e0924a02ca6
TL_Shambhavi_V <shambhavi.v@testyantra.in>
2022-05-24 11:10:44 +0530

    Add rate mrmrscart component

---
+++ pages/reseller/ratemrmrscart/index.js
@@ -0,0 +1,12 @@
+import { Paper, Typography } from "@mui/material";
+import RateMrMrsCartForm from "components/forms/reseller/ratemrmrscartform";
+const RateMrMrsCart = () => {
+  return (
+    <Paper sx={{ px: 5, py: 4, height: "100%", minHeight: "80vh" }}>
+      <Typography className="fs-14">Rate us here</Typography>
+      <RateMrMrsCartForm />
+    </Paper>
+  );
+};
+
+export default RateMrMrsCart;
