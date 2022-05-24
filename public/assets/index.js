5d82fa80231b4bd164c538295ce7464cf77951de
TY_RakeshKumar <rakesh.k@testyantra.in>
2022-05-24 17:07:55 +0530

    created assets json file

---
+++ public/assets/index.js
@@ -0,0 +1,67 @@
+export const images = {
+  add: "https://mrmrscart.s3.ap-south-1.amazonaws.com/APPLICATION-ASSETS/assets/img/add.png",
+  ecommerceBanner:
+    "https://mrmrscart.s3.ap-south-1.amazonaws.com/APPLICATION-ASSETS/assets/img/ecommerceBanner.jpg",
+  flower:
+    "https://mrmrscart.s3.ap-south-1.amazonaws.com/APPLICATION-ASSETS/assets/img/flower.jpg",
+  "grey-heart-icon":
+    "https://mrmrscart.s3.ap-south-1.amazonaws.com/APPLICATION-ASSETS/assets/img/grey-heart-icon.png",
+  "image-not-available":
+    "https://mrmrscart.s3.ap-south-1.amazonaws.com/APPLICATION-ASSETS/assets/img/image-not-available.jpg",
+  img_snap:
+    "https://mrmrscart.s3.ap-south-1.amazonaws.com/APPLICATION-ASSETS/assets/img/img_snap.PNG",
+  loginImage:
+    "https://mrmrscart.s3.ap-south-1.amazonaws.com/APPLICATION-ASSETS/assets/img/loginImage.jpg",
+  MultipleProducts:
+    "https://mrmrscart.s3.ap-south-1.amazonaws.com/APPLICATION-ASSETS/assets/img/MultipleProducts.png",
+  my: "https://mrmrscart.s3.ap-south-1.amazonaws.com/APPLICATION-ASSETS/assets/img/my.jpg",
+  NoPath:
+    "https://mrmrscart.s3.ap-south-1.amazonaws.com/APPLICATION-ASSETS/assets/img/NoPath.png",
+  person:
+    "https://mrmrscart.s3.ap-south-1.amazonaws.com/APPLICATION-ASSETS/assets/img/person.jpg",
+  "Printed Dress":
+    "https://mrmrscart.s3.ap-south-1.amazonaws.com/APPLICATION-ASSETS/assets/img/Printed+Dress.png",
+  "publish-coupon":
+    "https://mrmrscart.s3.ap-south-1.amazonaws.com/APPLICATION-ASSETS/assets/img/publish-coupon.PNG",
+  rating:
+    "https://mrmrscart.s3.ap-south-1.amazonaws.com/APPLICATION-ASSETS/assets/img/rating.png",
+  "red-heart-icon":
+    "https://mrmrscart.s3.ap-south-1.amazonaws.com/APPLICATION-ASSETS/assets/img/red-heart-icon.png",
+  redeem:
+    "https://mrmrscart.s3.ap-south-1.amazonaws.com/APPLICATION-ASSETS/assets/img/redeem.png",
+  registerConfirmIcon:
+    "https://mrmrscart.s3.ap-south-1.amazonaws.com/APPLICATION-ASSETS/assets/img/registerConfirmIcon.png",
+  saree_reseller_home:
+    "https://mrmrscart.s3.ap-south-1.amazonaws.com/APPLICATION-ASSETS/assets/img/saree_reseller_home.png",
+  SingleProduct:
+    "https://mrmrscart.s3.ap-south-1.amazonaws.com/APPLICATION-ASSETS/assets/img/SingleProduct.png",
+  spin1:
+    "https://mrmrscart.s3.ap-south-1.amazonaws.com/APPLICATION-ASSETS/assets/img/spin1.PNG",
+  spin3:
+    "https://mrmrscart.s3.ap-south-1.amazonaws.com/APPLICATION-ASSETS/assets/img/spin3.PNG",
+  spin4:
+    "https://mrmrscart.s3.ap-south-1.amazonaws.com/APPLICATION-ASSETS/assets/img/spin4.PNG",
+  spin5:
+    "https://mrmrscart.s3.ap-south-1.amazonaws.com/APPLICATION-ASSETS/assets/img/spin5.PNG",
+  splash:
+    "https://mrmrscart.s3.ap-south-1.amazonaws.com/APPLICATION-ASSETS/assets/img/splash.gif",
+  success:
+    "https://mrmrscart.s3.ap-south-1.amazonaws.com/APPLICATION-ASSETS/assets/img/success.png",
+  "upload image":
+    "https://mrmrscart.s3.ap-south-1.amazonaws.com/APPLICATION-ASSETS/assets/img/upload+image.png",
+  "facebook-circled":
+    "https://mrmrscart.s3.ap-south-1.amazonaws.com/APPLICATION-ASSETS/assets/icon/facebook-circled.png",
+  "facebook-messenger":
+    "https://mrmrscart.s3.ap-south-1.amazonaws.com/APPLICATION-ASSETS/assets/icon/facebook-messenger.png",
+  "instagram-logo":
+    "https://mrmrscart.s3.ap-south-1.amazonaws.com/APPLICATION-ASSETS/assets/icon/instagram-logo.png",
+  logo: "https://mrmrscart.s3.ap-south-1.amazonaws.com/APPLICATION-ASSETS/assets/icon/logo.png",
+  mrmrscartlogo:
+    "https://mrmrscart.s3.ap-south-1.amazonaws.com/APPLICATION-ASSETS/assets/icon/mrmrscartlogo.png",
+  "telegram-app":
+    "https://mrmrscart.s3.ap-south-1.amazonaws.com/APPLICATION-ASSETS/assets/icon/telegram-app.png",
+  "whatsapp-icon":
+    "https://mrmrscart.s3.ap-south-1.amazonaws.com/APPLICATION-ASSETS/assets/icon/watsapp-icon.png",
+  whatsapp:
+    "https://mrmrscart.s3.ap-south-1.amazonaws.com/APPLICATION-ASSETS/assets/icon/whatsapp.png",
+};
