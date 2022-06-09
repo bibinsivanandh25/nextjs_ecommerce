7da9838744d202f65ea310aa977dded7248cb1a1
Balu Murugesan <balu.m@testyantra.in>
2022-06-09 12:01:32 +0530

    added carousel component.

--- components/atoms/Carousel/index.js
+++ components/atoms/Carousel/index.js
@@ -1,12 +1,6 @@
-import React, { Component } from "react";
-import {
-  Carousel,
-  CarouselItem,
-  CarouselControl,
-  CarouselIndicators,
-  CarouselCaption,
-} from "reactstrap";
-
+import React from "react";
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 const items = [
   {
     src: "https://img.freepik.com/free-photo/front-view-cyber-monday-shopping-cart-with-bags-copy-space_23-2148657638.jpg?t=st=1652684330~exp=1652684930~hmac=2120a68ea587be22dc154a616865cfb21e67264ede09272e6eaa6dac18beb505&w=826",
@@ -25,90 +19,32 @@ const items = [
   },
 ];
 
-class CarousalComponent extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { activeIndex: 0 };
-    this.next = this.next.bind(this);
-    this.previous = this.previous.bind(this);
-    this.goToIndex = this.goToIndex.bind(this);
-    this.onExiting = this.onExiting.bind(this);
-    this.onExited = this.onExited.bind(this);
-  }
-
-  onExiting() {
-    this.animating = true;
-  }
-
-  onExited() {
-    this.animating = false;
-  }
-
-  next() {
-    if (this.animating) return;
-    const nextIndex =
-      this.state.activeIndex === items.length - 1
-        ? 0
-        : this.state.activeIndex + 1;
-    this.setState({ activeIndex: nextIndex });
-  }
-
-  previous() {
-    if (this.animating) return;
-    const nextIndex =
-      this.state.activeIndex === 0
-        ? items.length - 1
-        : this.state.activeIndex - 1;
-    this.setState({ activeIndex: nextIndex });
-  }
-
-  goToIndex(newIndex) {
-    if (this.animating) return;
-    this.setState({ activeIndex: newIndex });
-  }
-
-  render() {
-    const { activeIndex } = this.state;
-
-    const slides = items.map((item) => {
-      return (
-        <CarouselItem
-          onExiting={this.onExiting}
-          onExited={this.onExited}
-          key={item.src}
-        >
-          <img src={item.src} className="mxh-300 w-100" onClick={() => {}} />
-          <CarouselCaption />
-        </CarouselItem>
-      );
-    });
-
-    return (
-      <Carousel
-        autoplay={false}
-        activeIndex={activeIndex}
-        next={this.next}
-        previous={this.previous}
-      >
-        <CarouselIndicators
-          items={items}
-          activeIndex={activeIndex}
-          onClickHandler={this.goToIndex}
-        />
-        {slides}
-        <CarouselControl
-          direction="prev"
-          directionText="Previous"
-          onClickHandler={this.previous}
-        />
-        <CarouselControl
-          direction="next"
-          directionText="Next"
-          onClickHandler={this.next}
-        />
-      </Carousel>
-    );
-  }
+function CarousalComponent({
+  onChange = () => {},
+  onClickItem = () => {},
+  autoPlay = true,
+  interval = 2000,
+}) {
+  return (
+    <Carousel
+      showArrows={false}
+      onChange={onChange}
+      onClickItem={onClickItem}
+      autoPlay={autoPlay}
+      dynamicHeight={true}
+      infiniteLoop={true}
+      interval={interval}
+      showThumbs={false}
+    >
+      {items.map((value) => {
+        return (
+          <div>
+            <img src={value.src} alt="" style={{ maxHeight: "50vh" }} />
+          </div>
+        );
+      })}
+    </Carousel>
+  );
 }
 
 export default CarousalComponent;
