69423ad0094d5db0c26925d5bc982309df7c2290
TY_RakeshKumar <rakesh.k@testyantra.in>
2022-06-28 15:24:46 +0530

    Updated wishlist page in Customer Module

--- pages/customer/wishlist/index.js
+++ pages/customer/wishlist/index.js
@@ -1,8 +1,10 @@
-import { Box, Typography } from "@mui/material";
-import React from "react";
+/* eslint-disable react/no-array-index-key */
+import { Box, Paper, Typography } from "@mui/material";
+import React, { useEffect, useState } from "react";
+import Image from "next/image";
+import axios from "axios";
 import ButtonTabsList from "@/atoms/ButtonTabsList";
 import ButtonComponent from "@/atoms/ButtonComponent";
-import styles from "./wishlist.module.css";
 import StarRatingComponentReceivingRating from "@/atoms/StarRatingComponentReceiving";
 
 const WishList = () => {
@@ -11,10 +13,67 @@ const WishList = () => {
     { title: "Wishlist 2" },
     { title: "Wishlist 3" },
   ];
+  const [products, setProducts] = useState([]);
 
+  const getproducts = async () => {
+    await axios
+      .get("https://fakestoreapi.com/products")
+      .then((data) => {
+        setProducts([...data.data]);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  useEffect(() => {
+    getproducts();
+  }, []);
+  const getList = () => {
+    return products.map((ele, index) => {
+      return (
+        <Paper
+          className="d-flex justify-content-between ms-3 mb-2 rounded-1 align-items-center"
+          key={index}
+        >
+          <Box className="d-flex">
+            <Box>
+              <Image src={ele.image} height={100} width={100} />
+            </Box>
+            <Box className="ps-2">
+              <Typography className="fw-bold h-5">{ele.title}</Typography>
+              <StarRatingComponentReceivingRating rating={3} className="h-4" />
+              <Typography className="h-5">
+                {ele.rating.count} Reviews
+              </Typography>
+            </Box>
+          </Box>
+          <Box className="">
+            <Typography className="mb-1 text-center h-5">
+              Item added 2 Aug 2021
+            </Typography>
+            <Box className="mb-1">
+              <ButtonComponent
+                label="Add to cart"
+                muiProps="fw-bold fs-10 bg-primary w-100 px-5"
+                textColor="color-black"
+              />
+            </Box>
+            <Box className="mb-1">
+              <ButtonComponent
+                label="Remove from list"
+                muiProps="fw-bold fs-10 w-100 text-dark px-5"
+                textColor="text-dark"
+                bgColor="bg-white"
+              />
+            </Box>
+          </Box>
+        </Paper>
+      );
+    });
+  };
   return (
-    <Box className="d-flex align-items-start body-bg mnh-100vh p-4">
-      <Box className={`${styles.minHeight265} w-20p bg-white p-2 rounded-1`}>
+    <Box className="d-flex xjustify-content-between">
+      <Paper className="bg-white p-2 rounded-1 w-20p mb-2">
         <ButtonTabsList tabsList={list} />
         <Box className="mt-3">
           <ButtonComponent
@@ -25,49 +84,14 @@ const WishList = () => {
             textColor="color-orange"
           />
         </Box>
-      </Box>
-      <Box className="d-flex justify-content-between w-80p ms-4 bg-white p-2 rounded-1">
-        <Box className="w-70p">
-          <Box className="d-flex">
-            <Box className={`${styles.image250}`}>
-              <img
-                className="d-block w-100 h-100 img-fluid rounded-1"
-                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQXMWvWQyO6_SDsIxDQ33qBmWSgyUAiNPYfGY804qa6&s"
-                alt="product"
-              />
-            </Box>
-            <Box className="ps-2">
-              <Typography variant="h6" className="fw-bold mb-3">
-                Product Name
-              </Typography>
-              <StarRatingComponentReceivingRating rating={3} />
-              <Typography>581 Reviews</Typography>
-            </Box>
-          </Box>
-        </Box>
-        <Box className="w-30p">
-          <Typography className="mb-1 text-center">
-            Item added 2 Aug 2021
-          </Typography>
-          <Box className="mb-1">
-            <ButtonComponent
-              label="Add to cart"
-              variant="contained"
-              muiProps="fw-bold border border-secondary fs-12 bg-primary w-100"
-              borderColor="bg-light-gray"
-              textColor="color-black"
-            />
-          </Box>
-          <Box className="mb-1">
-            <ButtonComponent
-              label="Remove from list"
-              variant="outlined"
-              muiProps="fw-bold border border-secondary fs-12 w-100"
-              borderColor="bg-light-gray"
-              textColor="color-black"
-            />
-          </Box>
-        </Box>
+      </Paper>
+      <Box
+        className="w-100 overflow-y-scroll hide-scrollbar"
+        sx={{
+          maxHeight: "79vh !important",
+        }}
+      >
+        {getList()}
       </Box>
     </Box>
   );
