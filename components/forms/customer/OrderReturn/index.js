67c734e6b9adc5b676e16ff9657b0eeb15404f18
Balu Murugesan <balu.m@testyantra.in>
2022-07-06 15:43:28 +0530

    orderReturn page.

--- components/forms/customer/OrderReturn/index.js
+++ components/forms/customer/OrderReturn/index.js
@@ -1,13 +1,19 @@
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable jsx-a11y/no-static-element-interactions */
 /* eslint-disable consistent-return */
 /* eslint-disable no-return-assign */
 /* eslint-disable no-param-reassign */
 import { Box, Divider, Grid, Paper, Typography } from "@mui/material";
 import { useEffect, useState } from "react";
 import Image from "next/image";
+import CustomIcon from "services/iconUtils";
 import ButtonComponent from "@/atoms/ButtonComponent";
 import RadiobuttonComponent from "@/atoms/RadiobuttonComponent";
 
-const OrderReturn = ({ returnProducts = [] }) => {
+const OrderReturn = ({
+  returnProducts = [],
+  setShowReturnOrder = () => {},
+}) => {
   const [addressList, setAddressList] = useState([
     {
       id: 1,
@@ -182,151 +188,169 @@ const OrderReturn = ({ returnProducts = [] }) => {
     return temp;
   };
   return (
-    <Grid container>
-      {" "}
-      <Grid item sm={9}>
-        <Box className="mxh-79vh overflow-auto hide-scrollbar">
-          <Paper className="d-flex justify-content-between align-items-center px-2 rounded-0">
-            <Box className="d-flex my-2">
-              <Box>
-                <Typography className="mark me-3 py-1 px-2" component="span">
-                  1
-                </Typography>
-              </Box>
-              <Box>
-                <Typography className="text-secondary fw-bold h-5">
-                  Login
-                </Typography>
-                <Box>
-                  <Typography component="span" className="fw-bold h-5 me-3">
-                    User Name
-                  </Typography>
-                  <Typography
-                    component="span"
-                    className="text-secondary fw-bold h-5"
-                  >
-                    +91 123453453
-                  </Typography>
-                </Box>
-              </Box>
-            </Box>
-            <Box>
-              <ButtonComponent
-                label="Change"
-                muiProps="px-4"
-                variant="outlined"
-              />
-            </Box>
-          </Paper>
-          <Paper
-            className="my-2"
-            onClick={() => {
-              if (!showDeliveryAddress) {
-                setShowDeliveryAddress(true);
-              }
-            }}
-          >
-            <Box className="d-flex justify-content-between align-items-center px-2 rounded-0">
+    <Box>
+      <div
+        className="d-flex fw-600"
+        onClick={() => {
+          setShowReturnOrder(false);
+        }}
+      >
+        <CustomIcon type="keyboardBackspaceIcon" />
+        <p className="cursor-pointer">Back</p>
+      </div>
+      <Grid container>
+        <Grid item sm={9}>
+          <Box className="mxh-79vh overflow-auto hide-scrollbar">
+            <Paper className="d-flex justify-content-between align-items-center px-2 rounded-0">
               <Box className="d-flex my-2">
                 <Box>
                   <Typography className="mark me-3 py-1 px-2" component="span">
-                    2
+                    1
                   </Typography>
                 </Box>
                 <Box>
                   <Typography className="text-secondary fw-bold h-5">
-                    Pickup Address
+                    Login
                   </Typography>
+                  <Box>
+                    <Typography component="span" className="fw-bold h-5 me-3">
+                      User Name
+                    </Typography>
+                    <Typography
+                      component="span"
+                      className="text-secondary fw-bold h-5"
+                    >
+                      +91 123453453
+                    </Typography>
+                  </Box>
                 </Box>
               </Box>
               <Box>
-                {showDeliveryAddress ? (
-                  <ButtonComponent
-                    label="Continue"
-                    muiProps="px-4"
-                    onBtnClick={() => {
-                      setShowDeliveryAddress(false);
-                      setshowOrderSummary(true);
-                    }}
-                  />
-                ) : null}
+                <ButtonComponent
+                  label="Change"
+                  muiProps="px-4"
+                  variant="outlined"
+                />
               </Box>
-            </Box>
-            <Divider variant="middle" />
-            <Box className="py-2">{getDeliveryAddress()}</Box>
-          </Paper>
-          <Paper className="my-2">
-            <Box className="d-flex justify-content-between align-items-center px-2 rounded-0">
-              <Box className="d-flex my-2">
-                <Box>
-                  <Typography className="mark me-3 py-1 px-2" component="span">
-                    3
-                  </Typography>
-                </Box>
-                <Box>
-                  <Typography className="text-secondary fw-bold h-5">
-                    Return Order Summary
-                  </Typography>
-                </Box>
-              </Box>
-              <Box>
-                {showDeliveryAddress ? (
-                  <ButtonComponent
-                    label="Continue"
-                    muiProps="px-4"
-                    onBtnClick={() => setShowDeliveryAddress(false)}
-                  />
-                ) : null}
-              </Box>
-            </Box>
-            <Divider variant="middle" />
-            <Box className="py-2 mxh-300 overflow-auto hide-scrollbar">
-              {showOrderSummary ? getOrderSummary() : null}
-            </Box>
-          </Paper>
-
-          <Paper className="d-flex justify-content-between align-items-center p-2">
-            <Typography>
-              Return Order confirmation will be sent to{" "}
-              <span className="fw-bold">xyz.gmail.com</span>{" "}
-            </Typography>
-            <ButtonComponent
-              label="Continue"
-              onBtnClick={() => {
-                setShowOrderSuccessModal(true);
+            </Paper>
+            <Paper
+              className="my-2"
+              onClick={() => {
+                if (!showDeliveryAddress) {
+                  setShowDeliveryAddress(true);
+                }
               }}
-            />
-          </Paper>
-        </Box>
-      </Grid>
-      {products.length ? (
-        <Grid item sm={3}>
-          <Paper className="ms-2 p-2">
-            <Typography className="text-secondary h-5 fw-bold">
-              Return Order Charges
-            </Typography>
-            <Divider />
-            <Box className="d-flex justify-content-between align-items-center">
-              <Typography className="h-5  my-2">
-                Total item for return shipment
+            >
+              <Box className="d-flex justify-content-between align-items-center px-2 rounded-0">
+                <Box className="d-flex my-2">
+                  <Box>
+                    <Typography
+                      className="mark me-3 py-1 px-2"
+                      component="span"
+                    >
+                      2
+                    </Typography>
+                  </Box>
+                  <Box>
+                    <Typography className="text-secondary fw-bold h-5">
+                      Pickup Address
+                    </Typography>
+                  </Box>
+                </Box>
+                <Box>
+                  {showDeliveryAddress ? (
+                    <ButtonComponent
+                      label="Continue"
+                      muiProps="px-4"
+                      onBtnClick={() => {
+                        setShowDeliveryAddress(false);
+                        setshowOrderSummary(true);
+                      }}
+                    />
+                  ) : null}
+                </Box>
+              </Box>
+              <Divider variant="middle" />
+              <Box className="py-2">{getDeliveryAddress()}</Box>
+            </Paper>
+            <Paper className="my-2">
+              <Box className="d-flex justify-content-between align-items-center px-2 rounded-0">
+                <Box className="d-flex my-2">
+                  <Box>
+                    <Typography
+                      className="mark me-3 py-1 px-2"
+                      component="span"
+                    >
+                      3
+                    </Typography>
+                  </Box>
+                  <Box>
+                    <Typography className="text-secondary fw-bold h-5">
+                      Return Order Summary
+                    </Typography>
+                  </Box>
+                </Box>
+                <Box>
+                  {showDeliveryAddress ? (
+                    <ButtonComponent
+                      label="Continue"
+                      muiProps="px-4"
+                      onBtnClick={() => setShowDeliveryAddress(false)}
+                    />
+                  ) : null}
+                </Box>
+              </Box>
+              <Divider variant="middle" />
+              <Box className="py-2 mxh-300 overflow-auto hide-scrollbar">
+                {showOrderSummary ? getOrderSummary() : null}
+              </Box>
+            </Paper>
+
+            <Paper className="d-flex justify-content-between align-items-center p-2">
+              <Typography>
+                Return Order confirmation will be sent to{" "}
+                <span className="fw-bold">xyz.gmail.com</span>{" "}
               </Typography>
-              <Typography className="h-5 ">{products.length}</Typography>
-            </Box>
-            <Box className="d-flex justify-content-between">
-              <Typography className="h-5  my-2">
-                Return Shipment Charges
-              </Typography>
-              <Typography className="h-5  my-2">Rs.123</Typography>
-            </Box>
-            <Divider />
-            <Box className="d-flex justify-content-between align-items-center my-2">
-              <Typography className="h-5 fw-bold">Total payable</Typography>
-              <Typography className="h-5 fw-bold">{getTotalPrice()}</Typography>
-            </Box>
-          </Paper>
+              <ButtonComponent
+                label="Continue"
+                onBtnClick={() => {
+                  setShowOrderSuccessModal(true);
+                }}
+              />
+            </Paper>
+          </Box>
         </Grid>
-      ) : null}
-    </Grid>
+        {products.length ? (
+          <Grid item sm={3}>
+            <Paper className="ms-2 p-2">
+              <Typography className="text-secondary h-5 fw-bold">
+                Return Order Charges
+              </Typography>
+              <Divider />
+              <Box className="d-flex justify-content-between align-items-center">
+                <Typography className="h-5  my-2">
+                  Total item for return shipment
+                </Typography>
+                <Typography className="h-5 ">{products.length}</Typography>
+              </Box>
+              <Box className="d-flex justify-content-between">
+                <Typography className="h-5  my-2">
+                  Return Shipment Charges
+                </Typography>
+                <Typography className="h-5  my-2">Rs.123</Typography>
+              </Box>
+              <Divider />
+              <Box className="d-flex justify-content-between align-items-center my-2">
+                <Typography className="h-5 fw-bold">Total payable</Typography>
+                <Typography className="h-5 fw-bold">
+                  {getTotalPrice()}
+                </Typography>
+              </Box>
+            </Paper>
+          </Grid>
+        ) : null}
+      </Grid>
+    </Box>
   );
 };
 export default OrderReturn;
