35aa32a52314167de9358d5f5ab68a3f42cea5fd
TY_RakeshKumar <rakesh.k@testyantra.in>
2022-07-18 16:12:04 +0530

    created pie chart

---
+++ pages/admin/orders/dashboard/index.js
@@ -0,0 +1,10 @@
+import { PieChart } from "@/atoms/PieChart";
+
+const DashBoard = () => {
+  return (
+    <div className="w-25">
+      <PieChart />
+    </div>
+  );
+};
+export default DashBoard;
