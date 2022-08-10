8b8d247d26da48c6833e88f4894d3f8546dcec92
TY_RakeshKumar <rakesh.k@testyantra.in>
2022-08-10 14:50:43 +0530

    Integrated API and updated chnages in SUPPLIER

--- pages/auth/supplier/newpassword/index.js
+++ pages/auth/supplier/newpassword/index.js
@@ -1,7 +1,9 @@
 import { Grid, Typography } from "@mui/material";
 import NewPasswordForm from "components/forms/supplier/newpassword";
 import { useRouter } from "next/router";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import serviceUtil from "services/utils";
+import toastify from "services/utils/toastUtils";
 import styles from "./Newpassword.module.css";
 
 const Newpassword = () => {
@@ -11,11 +13,33 @@ const Newpassword = () => {
     rePassword: "",
   });
   const router = useRouter();
+
+  useEffect(() => {
+    setFormValues((pre) => ({
+      ...pre,
+      userId: router.query.user,
+    }));
+  }, [router.query.user]);
+
   // const [showModal, setShowModal] = useState(false);
   const handleSubmit = () => {
-    console.log(formValues);
+    serviceUtil
+      .post("users/registration/reset-password", {
+        userName: formValues.userId,
+        newPassword: formValues.password,
+        reEnterPassword: formValues.rePassword,
+        userType: "SUPPLIER",
+      })
+      .then((data) => {
+        toastify(data.data.message, "success");
+        if (data) {
+          router.push("/auth/login");
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
     // setShowModal(true);
-    router.push("/auth/login");
   };
   return (
     <Grid container spacing={2} className="">
