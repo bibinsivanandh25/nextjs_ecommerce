67d552c8f69adf638cf334b4177c2077ce984084
TY-AAYUSH-SOOD <aayush.s@testyantra.in>
2022-08-30 13:09:46 +0530

    admin/banners done

--- pages/admin/banners/index.js
+++ pages/admin/banners/index.js
@@ -1,66 +1,64 @@
-import { Box, Paper } from "@mui/material";
-import Image from "next/image";
 import React from "react";
-import CustomIcon from "services/iconUtils";
-import StarRatingComponentReceivingRating from "@/atoms/StarRatingComponentReceiving";
+import { Box, Paper, Typography } from "@mui/material";
+import Image from "next/image";
 import MenuOption from "@/atoms/MenuOptions";
+import SwitchComponent from "@/atoms/SwitchComponent";
 import TableComponent from "@/atoms/TableComponent";
 
-const Reviews = () => {
+const Banners = () => {
   const columns = [
     {
       id: "col1",
       align: "center",
-      label: "Customer/Reseller ID with Name",
+      label: "Banner Image",
       data_align: "center",
     },
     {
       id: "col2",
       align: "center",
-      label: "Email",
+      label: "Banner Image",
       data_align: "center",
     },
     {
       id: "col3",
       align: "center",
-      label: "Mobile",
+      label: "Display Page",
       data_align: "center",
     },
     {
       id: "col4",
       align: "center",
-      label: "Product Image",
+      label: "Navigation page URL",
       data_align: "center",
     },
     {
       id: "col5",
       align: "center",
-      label: "Product ID",
+      label: "Button",
       data_align: "center",
     },
     {
       id: "col6",
       align: "center",
-      label: "Rating",
+      label: "Created By",
       data_align: "center",
-      minWidth: "140px",
     },
     {
       id: "col7",
       align: "center",
-      label: "Comments",
+      label: "Created Date & Time",
       data_align: "center",
     },
     {
       id: "col8",
       align: "center",
-      label: "Supplier ID",
+      label: "Start Date & Time",
       data_align: "center",
     },
     {
       id: "col9",
       align: "center",
-      label: "Review Date & Time",
+      label: "End Date & Time",
       data_align: "center",
     },
     {
@@ -80,34 +78,30 @@ const Reviews = () => {
   const rows = [
     {
       id: 1,
-      col1: "#C8372493 – Raja Raman",
-      col2: "Home Page",
-      col3: "1232131232",
-      col4: (
+      col1: (
         <Image
           src="https://mrmrscart.s3.ap-south-1.amazonaws.com/APPLICATION-ASSETS/assets/img/ecommerceBanner.jpg"
           height={50}
           width={50}
         />
       ),
-      col5: "---",
-      col6: <StarRatingComponentReceivingRating className="h-5" rating={3} />,
-      col7: "Show any text added by customer or reseller",
-      col8: "#S827342 MS fahion",
+      col2: "Reseller",
+      col3: "Home Page",
+      col4: "---",
+      col5: "Shop Now",
+      col6: "#2374238 Arun Kumar",
+      col7: "23/06/2022 - 10.54",
+      col8: "23/06/2022 - 10.54",
       col9: "23/06/2022 - 10.54",
       col10: "Published",
       col11: (
         <Box className="d-flex align-items-center justify-content-around">
-          <CustomIcon
-            type="doneIcon"
-            showColorOnHover={false}
-            className="p-1 bg-success color-white rounded"
-          />
-          <CustomIcon
-            type="close"
-            showColorOnHover={false}
-            className="p-1 bg-danger color-white rounded ms-2"
-          />
+          <Box className="d-flex flex-column align-items-center">
+            <Box className="ms-4">
+              <SwitchComponent label="" />
+            </Box>
+            <Typography className="h-5">Disable</Typography>
+          </Box>
           <MenuOption
             getSelectedItem={(ele) => {
               console.log(ele);
@@ -130,10 +124,12 @@ const Reviews = () => {
         tableRows={rows}
         table_heading="Reviews"
         showDateFilter
+        showDateFilterBtn
+        dateFilterBtnName="Create Banners"
         showSearchbar
       />
     </Paper>
   );
 };
 
-export default Reviews;
+export default Banners;
