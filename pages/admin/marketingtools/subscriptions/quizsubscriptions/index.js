c418654f300e144bc4916c961aca49aff92d908c
TY-AAYUSH-SOOD <aayush.s@testyantra.in>
2022-09-28 12:32:56 +0530

    Quiz api integrated

--- pages/admin/marketingtools/subscriptions/quizsubscriptions/index.js
+++ pages/admin/marketingtools/subscriptions/quizsubscriptions/index.js
@@ -1,15 +1,27 @@
+/* eslint-disable no-use-before-define */
 import { Box, Paper, Typography } from "@mui/material";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import CustomIcon from "services/iconUtils";
 import MenuOption from "@/atoms/MenuOptions";
 import SwitchComponent from "@/atoms/SwitchComponent";
 import TableComponent from "@/atoms/TableWithSpan";
 import ViewModal from "@/forms/admin/marketingtools&subscriptions/quizsubscriptions/ViewModal";
 import AddNoteModal from "@/forms/admin/marketingtools&subscriptions/quizsubscriptions/AddNoteModal";
+import {
+  enableOrDisableSubscriptions,
+  getSubscriptions,
+} from "services/admin/marketingtools/subscriptions";
+import toastify from "services/utils/toastUtils";
+import CreateNotification from "@/forms/admin/marketingtools&subscriptions/quizsubscriptions/CreateNotificationModal";
 
-const SpinWheelSubscriptions = () => {
+const QuizSubscriptions = () => {
   const [openViewModal, setOpenViewModal] = useState(false);
   const [openAddNoteModal, setOpenAddNoteModal] = useState(false);
+  const [dataOfSingleSupplierOrReseller, setDataOfSingleSupplierOrReseller] =
+    useState([]);
+  const [showNotificationModal, setShowNotificationModal] = useState(false);
+
+  const [rowsForQuizSubs, setRowsForQuizSubs] = useState([]);
 
   const column1 = [
     {
@@ -82,7 +94,7 @@ const SpinWheelSubscriptions = () => {
   const column2 = [
     {
       id: "col3",
-      label: "--",
+      label: "7 Days",
       minWidth: 150,
       align: "center",
       data_align: "center",
@@ -91,7 +103,7 @@ const SpinWheelSubscriptions = () => {
     },
     {
       id: "col4",
-      label: "1/12/2021 - 12.25 to 30/12/2021 - 12.25",
+      label: "30 Days",
       minWidth: 150,
       align: "center",
       data_align: "center",
@@ -100,7 +112,7 @@ const SpinWheelSubscriptions = () => {
     },
     {
       id: "col5",
-      label: "--",
+      label: "90 Days",
       minWidth: 150,
       align: "center",
       data_align: "center",
@@ -109,7 +121,7 @@ const SpinWheelSubscriptions = () => {
     },
     {
       id: "col6",
-      label: "--",
+      label: "180 Days",
       minWidth: 150,
       align: "center",
       data_align: "center",
@@ -118,7 +130,7 @@ const SpinWheelSubscriptions = () => {
     },
     {
       id: "col7",
-      label: "--",
+      label: "270 Days",
       minWidth: 150,
       align: "center",
       data_align: "center",
@@ -127,7 +139,7 @@ const SpinWheelSubscriptions = () => {
     },
     {
       id: "col8",
-      label: "--",
+      label: "360 Days",
       minWidth: 150,
       align: "center",
       data_align: "center",
@@ -137,52 +149,170 @@ const SpinWheelSubscriptions = () => {
   ];
 
   const onClickOfMenuItem = (ele) => {
-    if (ele === "Add Note") {
-      setOpenAddNoteModal(true);
+    if (ele === "Add Note") setOpenAddNoteModal(true);
+
+    if (ele === "Notify") setShowNotificationModal(true);
+  };
+
+  const handleEnableOrDisable = async (purchaseId, status, marketingTool) => {
+    const { error } = await enableOrDisableSubscriptions(
+      purchaseId,
+      status,
+      marketingTool
+    );
+    if (!error) {
+      toastify(`${status ? "Disabled" : "Enabled"} successfully`, "success");
+      getQuizSubscription();
+    } else {
+      toastify(`Unable to change the status`, "error");
     }
   };
 
-  const rows = [
-    {
-      id: 1,
-      col1: "01",
-      col2: "#827342",
-      col3: "--",
-      col4: "1/12/2021 - 12.25 to 30/12/2021 - 12.25",
-      col5: "--",
-      col6: "--",
-      col7: "--",
-      col8: "--",
-      col9: "sdasdasd",
-      col10: "Active",
-      col11: 25,
-      col12: (
-        <Box className="d-flex justify-content-evenly align-items-center">
-          <CustomIcon
-            type="view"
-            className="fs-18"
-            onIconClick={() => setOpenViewModal(true)}
-          />
-          <MenuOption
-            getSelectedItem={(ele) => {
-              onClickOfMenuItem(ele);
-            }}
-            options={[
-              "Notify",
-              "Add Note",
-              <Box className="d-flex align-items-center">
-                <Typography>Disable</Typography>
-                <Box className="ms-4">
-                  <SwitchComponent label="" />
-                </Box>
-              </Box>,
-            ]}
-            IconclassName="fs-18 color-gray"
-          />
-        </Box>
-      ),
-    },
-  ];
+  // const rows = [
+  //   {
+  //     id: 1,
+  //     col1: "01",
+  //     col2: "#827342",
+  //     col3: "--",
+  //     col4: "1/12/2021 - 12.25 to 30/12/2021 - 12.25",
+  //     col5: "--",
+  //     col6: "--",
+  //     col7: "--",
+  //     col8: "--",
+  //     col9: "sdasdasd",
+  //     col10: "Active",
+  //     col11: 25,
+  //     col12: (
+  //       <Box className="d-flex justify-content-evenly align-items-center">
+  //         <CustomIcon
+  //           type="view"
+  //           className="fs-18"
+  //           onIconClick={() => setOpenViewModal(true)}
+  //         />
+  //         <MenuOption
+  //           getSelectedItem={(ele) => {
+  //             onClickOfMenuItem(ele);
+  //           }}
+  //           options={[
+  //             "Notify",
+  //             "Add Note",
+  //             <Box className="d-flex align-items-center">
+  //               <Typography>Disable</Typography>
+  //               <Box className="ms-4">
+  //                 <SwitchComponent label="" />
+  //               </Box>
+  //             </Box>,
+  //           ]}
+  //           IconclassName="fs-18 color-gray"
+  //         />
+  //       </Box>
+  //     ),
+  //   },
+  // ];
+
+  async function getQuizSubscription() {
+    const { data, error } = await getSubscriptions({
+      marketingTool: "QUIZ",
+      toolStatus: "ACTIVE",
+      userType: "SUPPLIER",
+    });
+    if (data) {
+      console.log(data);
+      const mappedArray = data.map((val, index) => {
+        const dateOne = new Date(val.activatedAt);
+        const dateTwo = new Date(val.expirationDate);
+        const timeDifference = dateTwo.getTime() - dateOne.getTime();
+        const divisor = 1000 * 60 * 60 * 24;
+        const numberOfDays = timeDifference / divisor;
+        return {
+          id: val.purchaseId,
+          col1: index >= 9 ? index + 1 : `0${index + 1}`,
+          col2: val.purchasedById,
+          col3:
+            numberOfDays === 7
+              ? `${val.activatedAt}-${val.expirationDate}`
+              : "--",
+          col4:
+            numberOfDays === 30
+              ? `${val.activatedAt}-${val.expirationDate}`
+              : "--",
+          col5:
+            numberOfDays === 90
+              ? `${val.activatedAt}-${val.expirationDate}`
+              : "--",
+          col6:
+            numberOfDays === 180
+              ? `${val.activatedAt}-${val.expirationDate}`
+              : "--",
+          col7:
+            numberOfDays === 270
+              ? `${val.activatedAt}-${val.expirationDate}`
+              : "--",
+          col8:
+            numberOfDays === 360
+              ? `${val.activatedAt}-${val.expirationDate}`
+              : "--",
+          col9: val.toolStatus,
+          col10: val.subscriptionAmount,
+          col11: val.comments ? val.comments : "0",
+          col12: (
+            <Box className="d-flex justify-content-evenly align-items-center">
+              <CustomIcon
+                type="view"
+                className="fs-18"
+                onIconClick={() => {
+                  console.log(
+                    "val.userMarketingTools ",
+                    val.userMarketingTools
+                  );
+                  setDataOfSingleSupplierOrReseller(val.userMarketingTools);
+                  setOpenViewModal(true);
+                }}
+              />
+              <MenuOption
+                getSelectedItem={(ele) => {
+                  console.log("Hey");
+                  onClickOfMenuItem(ele);
+                }}
+                options={[
+                  "Notify",
+                  "Add Note",
+                  <Box className="d-flex align-items-center">
+                    <Typography>
+                      {val.disabled ? "Disabled" : "Enabled"}
+                    </Typography>
+                    <Box className="ms-4">
+                      <SwitchComponent
+                        defaultChecked={!val.disabled}
+                        label=""
+                        ontoggle={() => {
+                          handleEnableOrDisable(
+                            val.purchaseId,
+                            !val.disabled,
+                            "TODAYS_DEAL"
+                          );
+                        }}
+                      />
+                    </Box>
+                  </Box>,
+                ]}
+                IconclassName="fs-18 color-gray"
+              />
+            </Box>
+          ),
+        };
+      });
+
+      setRowsForQuizSubs(mappedArray);
+    }
+    if (error) {
+      console.log("error hey", error);
+    }
+  }
+
+  useEffect(() => {
+    getQuizSubscription();
+  }, []);
 
   return (
     <>
@@ -194,7 +324,7 @@ const SpinWheelSubscriptions = () => {
           <TableComponent
             columns={[...column2]}
             column2={[...column1]}
-            tableRows={[...rows]}
+            tableRows={[...rowsForQuizSubs]}
             tHeadBgColor="bg-light-gray"
             showPagination={false}
             showSearchFilter={false}
@@ -209,13 +339,19 @@ const SpinWheelSubscriptions = () => {
       <ViewModal
         openViewModal={openViewModal}
         setOpenViewModal={setOpenViewModal}
+        dataOfSingleSupplierOrReseller={dataOfSingleSupplierOrReseller}
       />
       <AddNoteModal
         openAddNoteModal={openAddNoteModal}
         setOpenAddNoteModal={setOpenAddNoteModal}
       />
+      <CreateNotification
+        showNotificationModal={showNotificationModal}
+        setShowNotificationModal={setShowNotificationModal}
+        type="add"
+      />
     </>
   );
 };
 
-export default SpinWheelSubscriptions;
+export default QuizSubscriptions;
