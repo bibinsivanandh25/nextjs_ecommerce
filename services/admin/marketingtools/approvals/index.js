9214235ca75f2e42f4541c297c09332bd46a5495
TY_RakeshKumar <rakesh.k@testyantra.in>
2022-10-07 13:00:27 +0530

    integrated approve and reject api

--- services/admin/marketingtools/approvals/index.js
+++ services/admin/marketingtools/approvals/index.js
@@ -13,7 +13,7 @@ const getMarketingToolsBasedonMarketinType = (pageNumber, tool) => {
 
 const approveRejectMarketingToolCampaign = (payload) => {
   return serviceUtil
-    .put(`admin-marketing-tool/approve-reject-campaign`, payload)
+    .put(`users/admin-marketing-tool/approve-reject-campaign`, payload)
     .then((res) => res)
     .catch((err) => ({ err }));
 };
