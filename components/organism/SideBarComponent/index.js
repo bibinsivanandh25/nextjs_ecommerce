8fa34e4c9475e1a97a66f259ab8324ccaf2b9f25
TE_Suhil <suhil.km@testyantra.in>
2022-11-08 13:09:15 +0530

    updated sidebar for dynamic pages

--- components/organism/SideBarComponent/index.js
+++ components/organism/SideBarComponent/index.js
@@ -19,7 +19,7 @@ const SideBarComponent = ({ children }) => {
   const [showFallBack, setShowFallBack] = useState(false);
 
   useEffect(() => {
-    if (allowedPath) setShowFallBack(!allowedPath.includes(route.pathname));
+    if (allowedPath) setShowFallBack(!allowedPath.includes(route.asPath));
   }, [route.pathname, allowedPath]);
 
   return (
