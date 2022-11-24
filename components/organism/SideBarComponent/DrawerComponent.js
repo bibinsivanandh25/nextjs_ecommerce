f235a6728125684f4090cb313ffcc4c67f0af5c0
TE_Suhil <suhil.km@testyantra.in>
2022-11-24 20:29:32 +0530

    fixed lock icon issue in supplier marketing tool

--- components/organism/SideBarComponent/DrawerComponent.js
+++ components/organism/SideBarComponent/DrawerComponent.js
@@ -26,6 +26,7 @@ import { getmarketingToolStatus, getNavBarItems } from "services/supplier";
 import { setAllowedPaths, updateUnlockedTools } from "features/userSlice";
 import adminNav from "constants/adminNav";
 import Image from "next/image";
+import CustomIcon from "services/iconUtils";
 import CollapseList from "./CollapseList";
 
 const drawerWidth = 245;
@@ -483,7 +484,17 @@ const DrawerComponent = ({ open = false, setOpen = () => {} }) => {
                             fontSize={13}
                             // color={item.selected && "#e56700"}
                           >
-                            {item.title}
+                            <div className="d-flex justify-content-between">
+                              <Box
+                                id={item.id}
+                                className="fs-13 cursor-pointer"
+                              >
+                                {item.title}
+                              </Box>
+                              {item.locked && (
+                                <CustomIcon type="lock" className="fs-16" />
+                              )}
+                            </div>
                           </Typography>
                         }
                         sx={{
