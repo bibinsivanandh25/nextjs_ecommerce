d84fd94441c56a82e3112e088a434a10feb41786
TE_Suhil <suhil.km@testyantra.in>
2022-11-24 18:32:41 +0530

    fixed side bar issue

--- pages/auth/admin/login/otplogin.js
+++ pages/auth/admin/login/otplogin.js
@@ -45,7 +45,7 @@ const OtpLogIn = () => {
               lastName: data.lastName,
               profileImageUrl: data.profileImageUrl,
               supplierId: data.supplierId,
-              storeCode: data?.supplierStoreInfo?.supplierStoreCode || "",
+              storeCode: data.supplierStoreInfo.supplierStoreCode,
               isAddressSaved: data.userAddressDetails.length,
               role,
               storeName: data.supplierStoreInfo.supplierStoreName,
@@ -56,7 +56,7 @@ const OtpLogIn = () => {
               lastName: data.lastName,
               profileImageUrl: data.profileImageUrl,
               supplierId: data.supplierId,
-              storeCode: data?.supplierStoreInfo?.supplierStoreCode || "",
+              storeCode: data.supplierStoreInfo.supplierStoreCode,
               isAddressSaved: data.userAddressDetails.length,
               role,
               staffDetails: {
